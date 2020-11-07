92c53a7def0239df8e212cd86e7ad42a9717d9e8
SHIZA-gaming <65359498+SHIZA-gaming@users.noreply.github.com>
2020-11-07 12:24:22 +0300

    Update const.py

--- const.py
+++ const.py
@@ -1,3 +1,4 @@
 token = "1313759235:AAGCrz9wkBfmia0qxp2ud5Dc39GmRSe5GPQ"
 ip12_12mini_link = "https://www.apple.com/ru/shop/buy-iphone/iphone-12"
-ip12_link = "https://www.apple.com/ru/shop/buy-iphone/iphone-12-pro"
+ip12_12pro_link = "https://www.apple.com/ru/shop/buy-iphone/iphone-12-pro"
+ip11_link = "https://www.apple.com/ru/shop/buy-iphone/iphone-11"
